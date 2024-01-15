e1eb2308af5243db2c2532fb5cceed697902a2d4
SebastianZasadni <emblelele@gmail.com>
2024-01-15 19:18:20 +0100

    fix NavLink in logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,13 +1,14 @@
+import { NavLink } from "react-router-dom";
 import css from "./Logo.module.css";
 
 export const Logo = () => {
   return (
-    <a href="/main">
+    <NavLink to="/main">
       <img
         srcSet="/assets/icon-logo-header.png 1x, /assets/icon-logo-header@2x.png 2x"
         alt="logo"
         className={css.headerLogo}
       />
-    </a>
+    </NavLink>
   );
 };
