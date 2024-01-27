96f606f76f19a03af89558728a700abb997565a9
SebastianZasadni <emblelele@gmail.com>
2024-01-27 19:15:45 +0100

    delete unused import

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store.ts";
 import { PersistGate } from "redux-persist/integration/react";
-import App from "./App.tsx";
 import "./index.css";
 import AppWithSuspense from "./App.tsx";
 
